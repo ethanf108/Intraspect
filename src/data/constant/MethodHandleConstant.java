3f0fb6c5064d9bcb61a476ac33dd63c7d2a4566d
Ethan <ethanf108@gmail.com>
2022-01-20 16:14:36 -0500

    Added big giant validity checking to MethodHandleConstant

--- src/data/constant/MethodHandleConstant.java
+++ src/data/constant/MethodHandleConstant.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 
 public class MethodHandleConstant implements ConstantDesc {
 
-    private int kind;
+    private final int kind;
 
-    private int referenceIndex;
+    private final int referenceIndex;
 
     public MethodHandleConstant(int kind, int ref) {
         this.kind = kind;
@@ -31,7 +31,8 @@ public class MethodHandleConstant implements ConstantDesc {
 
     @Override
     public boolean isValid(ClassFile ref) {
-        return switch (kind) {
+        String methodName = null;
+        final boolean first = switch (kind) {
             case 1, 2, 3, 4 ->
                 ref.getConstandDesc(this.referenceIndex) instanceof FieldRefConstant;
             case 5, 8 ->
@@ -42,6 +43,38 @@ public class MethodHandleConstant implements ConstantDesc {
                 ref.getConstandDesc(this.referenceIndex) instanceof InterfaceMethodRefConstant;
             default ->
                 false;
+        };
+        return first && switch (kind) {
+            case 5,6,7,9:
+                if (ref.getConstandDesc(this.referenceIndex) instanceof InterfaceMethodRefConstant imrc) {
+                    if (ref.getConstandDesc(imrc.getNameAndTypeIndex()) instanceof NameAndTypeConstant natc
+                            && ref.getConstandDesc(natc.getNameIndex()) instanceof UTF8Constant u) {
+                        methodName = u.getValue();
+                    } else {
+                        yield false;
+                    }
+                }
+            case 8:
+                if (ref.getConstandDesc(this.referenceIndex) instanceof MethodRefConstant mrc) {
+                    if (ref.getConstandDesc(mrc.getNameAndTypeIndex()) instanceof NameAndTypeConstant natc
+                            && ref.getConstandDesc(natc.getNameIndex()) instanceof UTF8Constant u) {
+                        methodName = u.getValue();
+                    } else {
+                        yield false;
+                    }
+                }
+                if (methodName == null) {
+                    yield false;
+                }
+                if (kind == 8) {
+                    yield methodName.equals("<init>");
+                } else {
+                    yield !methodName.equals("<init>") && !methodName.equals("<clinit>");
+                }
+            case 1,2,3,4:
+                yield true;
+            default:
+                yield false;
         };
     }
 
