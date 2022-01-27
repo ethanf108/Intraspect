3e7bf2e809d8b829711b364082aed1798d6a45ce
Ethan <ethanf108@gmail.com>
2022-01-26 21:17:04 -0500

    Moved Constant Desc read to ConstantDesc

--- src/data/ClassFile.java
+++ src/data/ClassFile.java
@@ -1,6 +1,6 @@
 package data;
 
-import data.constant.*;
+import data.constant.EmptyWideConstant;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -33,38 +33,6 @@ public class ClassFile {
     }
 
     /**
-     * Reads a constant from the given data input stream.
-     *
-     * @param in the data input stream to read from
-     * @return the constant read from the data input stream
-     * @throws IOException              if an I/O error occurs
-     * @throws IllegalArgumentException if the constant is invalid
-     */
-    private static ConstantDesc readConstant(final DataInputStream in) throws IOException {
-        final int tag = in.readUnsignedByte();
-        return switch (tag) {
-            case 1 -> UTF8Constant.read(in);
-            case 3 -> IntegerConstant.read(in);
-            case 4 -> FloatConstant.read(in);
-            case 5 -> LongConstant.read(in);
-            case 6 -> DoubleConstant.read(in);
-            case 7 -> new ClassConstant(in.readUnsignedShort());
-            case 8 -> new StringConstant(in.readUnsignedShort());
-            case 9 -> new FieldRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 10 -> new MethodRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 11 -> new InterfaceMethodRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 12 -> new NameAndTypeConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 15 -> new MethodHandleConstant(in.readUnsignedByte(), in.readUnsignedShort());
-            case 16 -> new MethodTypeConstant(in.readUnsignedShort());
-            case 17 -> new DynamicConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 18 -> new InvokeDynamicConstant(in.readUnsignedShort(), in.readUnsignedShort());
-            case 19 -> new ModuleConstant(in.readUnsignedShort());
-            case 20 -> new PackageConstant(in.readUnsignedShort());
-            default -> throw new IllegalArgumentException("Invalid Tag: " + tag);
-        };
-    }
-
-    /**
      * Reads a class file from the given data input stream.
      *
      * @param in the data input stream to read from
@@ -83,7 +51,7 @@ public class ClassFile {
         final int constantPoolCount = in.readUnsignedShort();
         ret.constantPool = new ConstantDesc[constantPoolCount];
         for (int i = 1; i < constantPoolCount; i++) {
-            ret.constantPool[i] = readConstant(in);
+            ret.constantPool[i] = ConstantDesc.readConstant(in);
             if (ret.constantPool[i].isWide()) {
                 ret.constantPool[++i] = new EmptyWideConstant();
             }
--- src/data/ConstantDesc.java
+++ src/data/ConstantDesc.java
@@ -1,5 +1,8 @@
 package data;
 
+import data.constant.*;
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
@@ -7,6 +10,38 @@ import java.io.IOException;
  * An interface representing a constant descriptor.
  */
 public interface ConstantDesc {
+
+    /**
+     * Reads a constant from the given data input stream.
+     *
+     * @param in the data input stream to read from
+     * @return the constant read from the data input stream
+     * @throws IOException              if an I/O error occurs
+     * @throws IllegalArgumentException if the constant is invalid
+     */
+    static ConstantDesc readConstant(final DataInputStream in) throws IOException {
+        final int tag = in.readUnsignedByte();
+        return switch (tag) {
+            case 1 -> UTF8Constant.read(in);
+            case 3 -> IntegerConstant.read(in);
+            case 4 -> FloatConstant.read(in);
+            case 5 -> LongConstant.read(in);
+            case 6 -> DoubleConstant.read(in);
+            case 7 -> new ClassConstant(in.readUnsignedShort());
+            case 8 -> new StringConstant(in.readUnsignedShort());
+            case 9 -> new FieldRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 10 -> new MethodRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 11 -> new InterfaceMethodRefConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 12 -> new NameAndTypeConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 15 -> new MethodHandleConstant(in.readUnsignedByte(), in.readUnsignedShort());
+            case 16 -> new MethodTypeConstant(in.readUnsignedShort());
+            case 17 -> new DynamicConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 18 -> new InvokeDynamicConstant(in.readUnsignedShort(), in.readUnsignedShort());
+            case 19 -> new ModuleConstant(in.readUnsignedShort());
+            case 20 -> new PackageConstant(in.readUnsignedShort());
+            default -> throw new IllegalArgumentException("Invalid Tag: " + tag);
+        };
+    }
 
     int getTag();
 
