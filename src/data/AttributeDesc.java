aa3e34a90aff4152010c88044746477ae684c5fa
Ethan <ethanf108@gmail.com>
2022-02-03 14:02:55 -0500

    Added default attribute name method

--- src/data/AttributeDesc.java
+++ src/data/AttributeDesc.java
@@ -1,7 +1,10 @@
 package data;
 
+import data.constant.UTF8Constant;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Describes an attribute of a class.
@@ -13,4 +16,8 @@ public interface AttributeDesc {
     int getDataLength();
 
     void write(final DataOutputStream out) throws IOException;
+
+    default Optional<String> getAttributeName(ClassFile ref) {
+        return Optional.ofNullable(ref.getConstantDesc(this.getAttributeNameIndex()) instanceof UTF8Constant u ? u.getValue() : null);
+    }
 }
